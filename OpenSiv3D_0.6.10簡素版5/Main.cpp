ccc2430f5f75f40f65428d15a491caa7c34c5cfc
優士 小原 <230268@jc-21.jp>
2024-01-26 13:35:52 +0900

    プロジェクト ファイルを追加します。

---
+++ OpenSiv3D_0.6.10簡素版5/Main.cpp
@@ -0,0 +1,25 @@
+﻿# include <Siv3D.hpp> // OpenSiv3D v0.6.10
+
+void Main()
+{
+	// 背景の色を設定する | Set the background color
+	Scene::SetBackground(ColorF{ 0.6, 0.8, 0.7 });
+
+	// 絵文字からテクスチャを作成する | Create a texture from an emoji
+	const Texture emoji{ U"🦖"_emoji };
+
+	// 太文字のフォントを作成する | Create a bold font with MSDF method
+	const Font font{ FontMethod::MSDF, 48, Typeface::Bold };
+
+	// テキストに含まれる絵文字のためのフォントを作成し、font に追加する | Create a font for emojis in text and add it to font as a fallback
+	const Font emojiFont{ 48, Typeface::ColorEmoji };
+	font.addFallback(emojiFont);
+
+
+	while (System::Update())
+	{
+
+	}
+}
+
+
